49b52269286951e13624aa8404e5fbf8799672ea
passakorn_b <passakorn_b@mfec.co.th>
2022-05-31 23:24:12 +0700

    - add gitignore for VS - add matao ro custom item table - add pcall to add following custom item table

---
+++ Renewal/System/itemInfo_EN_MaTaO.lua
@@ -0,0 +1,23 @@
+return {
+		[1000975] = {
+		unidentifiedDisplayName = "Guillotine Cross Stone (Middle)",
+		unidentifiedResourceName = "¿µÈ¥ÀÇÁ¶°¢",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Guillotine Cross Stone (Middle)",
+		identifiedResourceName = "¿µÈ¥ÀÇÁ¶°¢",
+		identifiedDescriptionName = {
+			"A stone that awakens the potential of Guillotine Crosses.",
+			"Gives the following effects when compounded on a slot of an Costume Middle Headgear.",
+			"_______________________",
+			"For each Level of ^009900Dark Illusion^000000:",
+			"Increases Physical Melee Damage by 2%.",
+			"_______________________",
+			"<NAVI>[Aver De Dosh]<INFO>mal_in01,23,113,0,100,0,0</INFO></NAVI>",
+			"_______________________",
+			"^0000CCWeight:^000000 10"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+		},
+	}
